a17a4de2269a6ed0c6faab1d284c4ed7f7c54458
Fellipe Pontes <54919229+fellipepontesti@users.noreply.github.com>
2026-08-20 01:41:38 -0300

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        const hash = new Map<string, string[]>
+        for (let i = 0; i < strs.length; i++) {
+            const palavraOrdenada = strs[i].split('').sort().join('')
+
+            const result = hash.get(palavraOrdenada)
+            if (!result) {
+                hash.set(palavraOrdenada, [strs[i]])
+            } else {
+                result.push(strs[i])
+            }
+        }
+
+        return Array.from(hash.values())
+    }
+
+}
